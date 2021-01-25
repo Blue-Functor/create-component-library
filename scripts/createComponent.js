46fa1abb338d8462e1bf4de6a40fed11534e83ae
georgelivas <giorgos.livas.99@gmail.com>
2021-01-25 20:36:58 +0000

    add build script to new component

--- scripts/createComponent.js
+++ scripts/createComponent.js
@@ -9,16 +9,20 @@ if (componentNames.length < 1) {
     process.exit(-1)
 }
 
-const createComponent = name => {
+const createComponent = (name, componentName) => {
     execSync(`lerna create ${name} --yes`);
     console.log(name, ' Created!');
+    console.log('Please Wait. Installing dependencies and connecting components...')
     execSync(`lerna add @bluefunctor/library-builder --dev --scope=${name}`);
+    const packageJson = JSON.parse(fs.readFileSync(`packages/${config.libraryName}-${componentName}/package.json`));
+    packageJson.scripts.build = 'library-builder';
+    fs.writeFileSync(`packages/${config.libraryName}-${componentName}/package.json`, JSON.stringify(packageJson));
     execSync(`lerna add react --dev --scope=${name}`);
     execSync(`lerna add react@17.x --peer --scope=${name}`);
     execSync(`lerna add ${name} --scope=${config.npmOrg}/${config.libraryName}`);
     fs.appendFileSync(`packages/${config.libraryName}/lib/${config.libraryName}.js`, `export * from '${name}';`);
 };
 
-componentNames.map(name => createComponent(`${config.npmOrg}/${config.libraryName}-${name}`));
+componentNames.map(name => createComponent(`${config.npmOrg}/${config.libraryName}-${name}`, name));
 
 exports.createComponent = createComponent;
