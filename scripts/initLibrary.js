0031f58a038581a996dbdb580e41462f3ed03e99
georgelivas <giorgos.livas.99@gmail.com>
2021-01-27 20:57:06 +0000

    improve scripts

--- scripts/initLibrary.js
+++ scripts/initLibrary.js
@@ -1,13 +1,43 @@
+const fs = require('fs');
 const { execSync } = require('child_process');
+const Ora = require('ora');
+
 const config = require('../library.json');
 
 if (!config.libraryName || !config.npmOrg ) {
     console.error('ERROR: Please specify libraryName and npmOrg in the /library.json file.');
     process.exit(-1)
 }
+const initLib = async () => {
+    const spinner = new Ora({
+        discardStdin: false,
+        text: 'Creating package',
+    });
+    spinner.color = 'blue';
 
+    spinner.start();
+    execSync(`lerna create ${config.npmOrg}/${config.libraryName} --yes`);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    spinner.succeed();
+    spinner.text = 'Initialising variables';
+    spinner.start();
+    const libPath = `packages/${config.libraryName}`;
+    const packagePath = `${libPath}/package.json`;
+    const mainFile = `${libPath}/lib/${config.libraryName}.js`;
+    spinner.succeed();
+    spinner.text = 'Initialising files';
+    spinner.start();
 
-execSync(`lerna bootstrap`);
+    const packageJson = JSON.parse(fs.readFileSync(packagePath));
+    packageJson.description = 'This is the main Library package, all components will be exported from here.';
+    packageJson.publishConfig = {access: "public"};
+    fs.writeFileSync(packagePath, JSON.stringify(packageJson));
 
-execSync(`lerna create ${config.npmOrg}/${config.libraryName} --yes`);
-console.log('Library Created!');
+    fs.writeFileSync(mainFile, '');
+    spinner.succeed();
+    spinner.text = 'Library Created!';
+    spinner.start();
+    spinner.succeed();
+};
+
+initLib();
